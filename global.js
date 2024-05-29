82c0ac28b89d54bcbcc1b461d0a997b4d16de172
hekichien <chienlx1005@gmail.com>
2024-05-29 14:57:30 +0700

    a

--- global.js
+++ global.js
@@ -1,2 +1,18 @@
-
-// drawer
+class HelloWord extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode: "open"})
+    this.shadowRoot.innerHTML = `<h1>Heloo world</h1><button>Hello</button>
+    <slot></slot>
+    `
+  }
+  connectedCallback(){
+    this.shadowRoot.querySelector('button').addEventListener('click',(e)=>{
+      alert("click button");
+    });
+  }
+  disconnectedCallback(){
+    alert("This components removed! ")
+  }
+}
+customElements.define('hello-world',HelloWord)
